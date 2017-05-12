53cb446cfa63f26826562446ff9e48311e202b71
HeejuYoon <pokok69@naver.com>
2017-05-12 15:29:05 +0900

    조도센서 측정.
    조도센서 측정 값에 따라 3색 led등 불켜기

--- light.ino
+++ light.ino
@@ -1,3 +1,7 @@
+#define LIGHT 13
+#define RED 11
+#define GREEN 10
+#define BLUE 9
 
 static int light_warning_count; 
 
@@ -17,14 +21,21 @@ static int
 light_get()
 {
   //조도 센서로부터 받은 값을 리턴.
+  return analogRead(LIGHT);
 }
 
 int
 enough_light_is()
 {
+  if(light_get()<50)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
   //조도 센서의 측정값이 일정수준 아래로 떨어지면 0, 아니면 1 리턴.
-  //10분 단위로 측정하며 수준 아래로 떨어지면 카운트 플러스, 일정카운트 이상 쌓이면 0.
-  //수준 이상이 되면 카운트 초기화. 방열패드 off
 }
 
 void
@@ -34,8 +45,60 @@ light_warn()
 }
 
 void
-light_setup()
+light_check()
 {
   
+  if(enough_light_is()==0)
+  {
+    light_warning_count++;
+  }
+  else
+  {
+    light_warning_count=0;
+    plants_led_off();
+  }
+
+
+  if(light_warning_count>=6)
+  {
+    plants_led_on();
+  }
+  
+  //수준 아래로 떨어지면 카운트 플러스, 일정카운트 이상 쌓이면 0.
+  //수준 이상이 되면 카운트 초기화. 방열패드 off
 }
 
+void
+light_setup()
+{
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void
+led_3color_on()
+{
+  //조도센서 측정값에 따라 3색led등 on, off
+  if(light_warning_count>=6)
+  {
+    analogWrite(BLUE, 0);
+    analogWrite(GREEN, 0);
+    analogWrite(RED, 255);
+  }
+
+  else if(light_warning_count>=3 && light_warning_count<6)
+  {
+    analogWrite(RED, 0);
+    analogWrite(BLUE, 0);
+    analogWrite(GREEN, 255);
+  }
+
+  else
+  {
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 255);
+  }
+  
+}
