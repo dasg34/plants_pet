28b0001b8ff631b5038109e0357da586d311ed67
HeejuYoon <pokok69@naver.com>
2017-05-12 14:01:25 +0900

    조도센서의 함수를 정의함.

--- light.ino
+++ light.ino
@@ -1,3 +1,38 @@
+
+static int light_warning_count; 
+
+static void
+plants_led_on()
+{
+  
+}
+
+static void
+plants_led_off()
+{
+  
+}
+
+static int
+light_get()
+{
+  //조도 센서로부터 받은 값을 리턴.
+}
+
+int
+enough_light_is()
+{
+  //조도 센서의 측정값이 일정수준 아래로 떨어지면 0, 아니면 1 리턴.
+  //10분 단위로 측정하며 수준 아래로 떨어지면 카운트 플러스, 일정카운트 이상 쌓이면 0.
+  //수준 이상이 되면 카운트 초기화. 방열패드 off
+}
+
+void
+light_warn()
+{
+  //light on
+}
+
 void
 light_setup()
 {
